d37d1cbe90eef4c076f098a2eadde77f715a95af
ilwad123 <ilwadabdi234@gmail.com>
2025-05-16 22:22:17 +0100

    updated python clean

--- clean_hr_dataset.py
+++ clean_hr_dataset.py
@@ -13,12 +13,10 @@ df['YearsWithCurrManager'].fillna(-1, inplace=True)
 
 #check for duplictes in the employee id column
 duplicates = df.duplicated(subset=['EmpID'])
-print("number of duplicates in df:", duplicates.sum())
 #drop employee count column as it is not useful for analysis as it is constant value
 if df['EmployeeCount'].nunique() == 1:
     df.drop(columns=['EmployeeCount'], inplace=True)
 
-df.drop(columns=['Over18'], inplace=True)
 
 # df.drop_duplicates(subset=['EmpID'], keep='first', inplace=True)
 
@@ -29,6 +27,7 @@ dupes.to_csv("potential_conflicts.csv", index=False)
 #drop duplicate rows as there are duplicates because of empID is not all unique 
 #keep the first occurrence of each duplicate
 df.drop_duplicates(subset='EmpID', keep='first', inplace=True)
+df.drop(columns=['Over18'], inplace=True)
 df.to_csv("cleaned_data.csv", index=False)
 
 
